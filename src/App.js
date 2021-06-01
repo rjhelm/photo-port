fb1f2767aa48653be2e5ffed2fa8bf39ba357b7c
Ryan Helm <rjhelm@github.io>
2021-05-31 21:05:34 -0600

    code cleanup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import './App.css';
+import Nav from './components/Nav';
 import About from './components/About';
-import Nav from './components/Nav'
 
 function App() {
+
   return (
     <div>
-    <Nav></Nav>
+      <Nav />
       <main>
-        <About></About>
+          <About />        
       </main>
     </div>
   );
