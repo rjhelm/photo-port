42af0f154ad34c4c003a917436a96e51244e3764
Ryan Helm <rjhelm@github.io>
2021-06-03 17:18:56 -0600

    code cleanup

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React from 'react';
+import PhotoList from '../PhotoList';
 import { capitalizeFirstLetter } from '../../utils/helpers';
-import PhotoList from "./PhotoList";
 
-function Gallery({ currentCategory }) {
-    const { name, description } = currentCategory;
+function Gallery(props) {
+  const { currentCategory } = props;
   return (
     <section>
-      <h1 data-test-id="h1tag">{capitalizeFirstLetter(name)}</h1>
-      <p>{description}</p>
+      <h1 data-testid="h1tag">{capitalizeFirstLetter(currentCategory.name)}</h1>
+      <p>{currentCategory.description}</p>
       <PhotoList category={currentCategory.name} />
     </section>
   );
